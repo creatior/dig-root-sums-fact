2ff369b2d88b3a30a3fe9fb81d54abae735e3ed5
creatior <vartem2020@mail.ru>
2025-04-01 21:03:58 +0300

    Factorization of number

---
+++ Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+
+let factorizations n =
+    let rec factorize start n current acc =
+        match n with
+        | 1 -> [current] @ acc
+        | _ ->
+            [start .. n]
+            |> List.filter (fun x -> n % x = 0)
+            |> List.fold (fun a x -> 
+                match x with
+                | 1 -> a
+                | _ -> factorize x (n / x) (x::current) a) acc
+    
+    match n with
+    | n when n < 2 -> [[]]
+    | _ -> factorize 2 n [] []
+
+[<EntryPoint>]
+let main args = 
+    let result = factorizations 24
+    printfn "%A" result
+    0
